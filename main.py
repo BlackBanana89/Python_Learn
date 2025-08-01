4a2eb0a523945d196733bbf64e72e2f1afa558da
BlackBanana89 <dextermorgan@posteo.de>
2025-08-01 14:49:03 +0200

    main.py

--- main.py
+++ main.py
@@ -16,5 +16,3 @@ def calculate(list):
   'sum': [calc_array.sum(axis=0).tolist(), calc_array.sum(axis=1).tolist(), calc_array.flatten().sum().tolist()]
 }
     return calculations
-
-calculate ([0,1,2,3,4,5,6,7,8])
